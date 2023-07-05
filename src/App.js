1308e61133fe60bb2170260b7babea20707c5288
Vitor Grillo <vgrillo@its.jnj.com>
2023-07-05 17:55:42 -0300

    Implementing Figure Component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
+    </>
+  );
+}
+
+export default App;
